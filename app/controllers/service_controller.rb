a5955f04426a1bc51df0fd18cf5e90f01189c04f
Ayaz Khan <ayaz.ahmed@gaditek.com>
2013-06-18 17:35:38 +0500

    Added params.json and ServiceHelper to parse JSON params. Added Params support in Service actions.

--- app/controllers/service_controller.rb
+++ app/controllers/service_controller.rb
@@ -2,131 +2,142 @@
 require 'mcollective'
 include MCollective::RPC
 
+include ServiceHelper
+
 class ServiceController < ApplicationController
     http_basic_authenticate_with :name => "cloudways-dev-api", :password => "cloudways123+"
+    before_filter :init
+
+    def init 
+        @params_verifier = ParamsVerifier.new()
+        @service_name = nil
+        @customer_number = nil
+        @response = {:status => 0, :msg => ""}
+        @is_clean = true
+    end
+
+    def check_params
+        @is_clean = false
+        @service_name = params[:service_name]
+        @customer_number = params[:customer_number]
+
+        if @service_name.nil?
+            @response[:status] = -1
+            @response[:msg] = "Service name parameter missing."
+            return @response
+        end
+
+        if @customer_number.nil?
+            @response[:status] = -1
+            @response[:msg] = "Customer number parameter missing."
+            return @response
+        end
+
+        @service_name = @params_verifier.get_service(@service_name)
+        if @service_name.nil?
+            @response[:status] = -1
+            @response[:msg] = "Incorrect service name provided."
+            return @response
+        end
+
+        @customer_number = @params_verifier.get_customer_number(@customer_number)
+        if @customer_number.nil?
+            @response[:status] = -1
+            @response[:msg] = "Incorrect customer number provided."
+            return @response
+        end
+
+        @is_clean = true
+        @response
+    end
+
 
     def status
-        response = {:status => 0, :msg => ""}
-        service_name = params[:service_name]
-        customer_number = params[:customer_number]
-
-        if service_name.nil?
-            response[:status] = -1
-            response[:msg] = "Service name parameter missing."
-            return :json => response
+        @response = check_params
+        unless @is_clean
+            return render :json => @response
         end
 
-        rpc_client = rpcclient('service')
-        rpc_client.verbose = false
-        unless customer_number.nil?
-            rpc_client.fact_filter "cloudways_customer", customer_number
+        begin
+            rpc_client = rpcclient('service')
+            rpc_client.verbose = false
+            unless @customer_number.nil?
+                rpc_client.fact_filter "cloudways_customer", @customer_number
+            end
+            rpc_response = rpc_client.status(:service => @service_name)
+            @response = rpc_response
+        rescue Exception => e
+            @response[:status] = -2
+            @response[:msg] = "Server error: #{e}"
         end
-        rpc_response = rpc_client.status(:service => service_name)
-        response = rpc_response
 
-        render :json => response
+        render :json => @response
     end
 
 
     def start
-        response = {}
-        service_name = params[:service_name]
-        customer_number = params[:customer_number]
-
-        unless service_name.nil?
-            rpc_client = rpcclient('service')
-            rpc_client.verbose = false
-
-            unless customer_number.nil? 
-                rpc_client.fact_filter "cloudways_customer", customer_number
-            end
-            rpc_response = rpc_client.start(:service => service_name)
-            response = rpc_response
-        else
-            response[:status] = -1
-            response[:msg] = 'Service name parameter missing.'
+        @response = check_params
+        unless @is_clean
+            return render :json => @response
         end
 
-        render :json => response
+        begin
+            rpc_client = rpcclient('service')
+            rpc_client.verbose = false
+            unless @customer_number.nil?
+                rpc_client.fact_filter "cloudways_customer", @customer_number
+            end
+            rpc_response = rpc_client.start(:service => @service_name)
+            @response = rpc_response
+        rescue Exception => e
+            @response[:status] = -2
+            @response[:msg] = "API error: #{e}"
+        end
+
+        render :json => @response
     end
 
     def stop
-        response = {}
-        service_name = params[:service_name]
-        customer_number = params[:customer_number]
-
-        unless service_name.nil?
-            rpc_client = rpcclient('service')
-            rpc_client.verbose = false
-
-            unless customer_number.nil? 
-                rpc_client.fact_filter "cloudways_customer", customer_number
-            end
-            rpc_response = rpc_client.stop(:service => service_name)
-            response = rpc_response
-        else
-            response[:status] = -1
-            response[:msg] = 'Service name parameter missing.'
+        @response = check_params
+        unless @is_clean
+            return render :json => @response
         end
 
-        render :json => response
+        begin
+            rpc_client = rpcclient('service')
+            rpc_client.verbose = false
+            unless @customer_number.nil?
+                rpc_client.fact_filter "cloudways_customer", @customer_number
+            end
+            rpc_response = rpc_client.stop(:service => @service_name)
+            @response = rpc_response
+        rescue Exception => e
+            @response[:status] = -2
+            @response[:msg] = "API error: #{e}"
+        end
+
+        render :json => @response
     end
 
     def restart
-        response = {}
-        service_name = params[:service_name]
-        customer_number = params[:customer_number]
-
-        unless service_name.nil?
-            rpc_client = rpcclient('service')
-            rpc_client.verbose = false
-
-            unless customer_number.nil? 
-                rpc_client.fact_filter "cloudways_customer", customer_number
-            end
-            rpc_response = rpc_client.restart(:service => service_name)
-            response = rpc_response
-        else
-            response[:status] = -1
-            response[:msg] = 'Service name parameter missing.'
+        @response = check_params
+        unless @is_clean
+            return render :json => @response
         end
 
-        render :json => response
-    end
+        begin
+            rpc_client = rpcclient('service')
+            rpc_client.verbose = false
+            unless @customer_number.nil?
+                rpc_client.fact_filter "cloudways_customer", @customer_number
+            end
+            rpc_response = rpc_client.restart(:service => @service_name)
+            @response = rpc_response
+        rescue Exception => e
+            @response[:status] = -2
+            @response[:msg] = "API error: #{e}"
+        end
 
-    #  private
-    #  def perform_action(action)
-    #    service_name = params[:service_name]
-    #    customer_number = params[:customer_number]
-    #
-    #    rpc_resposne = {}
-    #
-    #    unless service_name.nil?
-    #      rpc_client = rpcclient('service')
-    #      rpc_client.verbose = false
-    #
-    #      unless customer_number.nil? 
-    #        rpc_client.fact_filter "cloudways_customer", customer_number
-    #      end
-    #
-    #      case action
-    #      when "start"
-    #        rpc_response = rpc_client.start(:service => service_name)
-    #      when "stop"
-    #        rpc_response = rpc_client.stop(:service => service_name)
-    #      when "restart"
-    #        rpc_response = rpc_client.restart(:service => service_name)
-    #      when "status"
-    #        rpc_response = rpc_client.status(:service => service_name)
-    #      else
-    #        rpc_response = {}
-    #      end
-    #    else
-    #      rpc_response[:status] = -1
-    #      rpc_response[:msg] = 'Service name parameter missing.'
-    #    end
-    #
-    #    return rpc_response
-    #  end
-    #  #private_class_method :perform_action
+        render :json => @response
+    end
 end
